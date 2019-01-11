c1419d553d79d848f183cf917e2b8f2193fbbdcb
Jeffri Sandy <jeffri.sandy@gmail.com>
2019-01-11 13:12:59 +0700

    add part 3 for supervised machine learning model notebook

--- helpers.py
+++ helpers.py
@@ -677,3 +677,203 @@ def load(filename):
     """
     import pickle
     return pickle.load(open(filename, 'rb'))
+
+"""
+##################################
+SPOT CHECK ML Supervised Alogrithm
+
+ref : https://machinelearningmastery.com/spot-check-machine-learning-algorithms-in-python/
+##################################
+"""
+
+import warnings
+from numpy import mean
+from numpy import std
+from matplotlib import pyplot
+from sklearn.datasets import make_regression
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import HuberRegressor
+from sklearn.linear_model import Lars
+from sklearn.linear_model import LassoLars
+from sklearn.linear_model import PassiveAggressiveRegressor
+from sklearn.linear_model import RANSACRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import TheilSenRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import ExtraTreeRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.multioutput import MultiOutputRegressor
+
+
+# create a dict of standard models to evaluate {name:object}
+def get_models(models=dict()):
+	# linear models
+	models['lr'] = LinearRegression()
+	alpha = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	for a in alpha:
+		models['lasso-'+str(a)] = Lasso(alpha=a)
+	for a in alpha:
+		models['ridge-'+str(a)] = Ridge(alpha=a)
+	for a1 in alpha:
+		for a2 in alpha:
+			name = 'en-' + str(a1) + '-' + str(a2)
+			models[name] = ElasticNet(a1, a2)
+	models['huber'] = HuberRegressor()
+	models['lars'] = Lars()
+	models['llars'] = LassoLars()
+	models['pa'] = PassiveAggressiveRegressor(max_iter=1000, tol=1e-3)
+	models['ranscac'] = RANSACRegressor()
+	models['sgd'] = SGDRegressor(max_iter=1000, tol=1e-3)
+	models['theil'] = TheilSenRegressor()
+	# non-linear models
+	n_neighbors = range(1, 21)
+	for k in n_neighbors:
+		models['knn-'+str(k)] = KNeighborsRegressor(n_neighbors=k)
+	models['cart'] = DecisionTreeRegressor()
+	models['extra'] = ExtraTreeRegressor()
+	models['svml'] = SVR(kernel='linear')
+	models['svmp'] = SVR(kernel='poly')
+	c_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	for c in c_values:
+		models['svmr'+str(c)] = SVR(C=c)
+	# ensemble models
+	n_trees = 100
+	models['ada'] = AdaBoostRegressor(n_estimators=n_trees)
+	models['bag'] = BaggingRegressor(n_estimators=n_trees)
+	models['rf'] = RandomForestRegressor(n_estimators=n_trees)
+	models['et'] = ExtraTreesRegressor(n_estimators=n_trees)
+	models['gbm'] = GradientBoostingRegressor(n_estimators=n_trees)
+	print('Defined %d models' % len(models))
+	return models
+
+# create a dict of standard models to evaluate {name:object} for MultiOutputRegressor
+def get_models_multioutput(models=dict()):
+    # linear models
+    models['lr'] = MultiOutputRegressor(LinearRegression())
+    alpha = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    for a in alpha:
+        models['lasso-'+str(a)] = MultiOutputRegressor(Lasso(alpha=a))
+    for a in alpha:
+        models['ridge-'+str(a)] = MultiOutputRegressor(Ridge(alpha=a))
+    for a1 in alpha:
+        for a2 in alpha:
+            name = 'en-' + str(a1) + '-' + str(a2)
+            models[name] = MultiOutputRegressor(ElasticNet(a1, a2))
+    models['huber'] = MultiOutputRegressor(HuberRegressor())
+    models['lars'] = MultiOutputRegressor(Lars())
+    models['llars'] = MultiOutputRegressor(LassoLars())
+    models['pa'] = MultiOutputRegressor(PassiveAggressiveRegressor(max_iter=1000, tol=1e-3))
+    models['ranscac'] = MultiOutputRegressor(RANSACRegressor())
+    models['sgd'] = MultiOutputRegressor(SGDRegressor(max_iter=1000, tol=1e-3))
+    models['theil'] = MultiOutputRegressor(TheilSenRegressor())
+    # non-linear models
+    n_neighbors = range(1, 21)
+    for k in n_neighbors:
+        models['knn-'+str(k)] = MultiOutputRegressor(KNeighborsRegressor(n_neighbors=k))
+    models['cart'] = MultiOutputRegressor(DecisionTreeRegressor())
+    models['extra'] = MultiOutputRegressor(ExtraTreeRegressor())
+    models['svml'] = MultiOutputRegressor(SVR(kernel='linear'))
+    models['svmp'] = MultiOutputRegressor(SVR(kernel='poly'))
+    c_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    for c in c_values:
+        models['svmr'+str(c)] = SVR(C=c)
+    # ensemble models
+    n_trees = 100
+    models['ada'] = MultiOutputRegressor(AdaBoostRegressor(n_estimators=n_trees))
+    models['bag'] = MultiOutputRegressor(BaggingRegressor(n_estimators=n_trees))
+    models['rf'] = MultiOutputRegressor(RandomForestRegressor(n_estimators=n_trees))
+    models['et'] = MultiOutputRegressor(ExtraTreesRegressor(n_estimators=n_trees))
+    models['gbm'] = MultiOutputRegressor(GradientBoostingRegressor(n_estimators=n_trees))
+    print('Defined %d models' % len(models))
+    return models
+
+# create a feature preparation pipeline for a model
+def make_pipeline(model):
+    steps = list()
+    # standardization
+    steps.append(('standardize', StandardScaler()))
+    # normalization
+    steps.append(('normalize', MinMaxScaler()))
+    # the model
+    steps.append(('model', model))
+    # create pipeline
+    pipeline = Pipeline(steps=steps)
+    return pipeline
+
+# evaluate a single model
+def evaluate_model(X, y, model, folds, metric):
+    # create the pipeline
+    pipeline = make_pipeline(model)
+    # evaluate model
+    scores = cross_val_score(pipeline, X, y, scoring=metric, cv=folds, n_jobs=-1)
+    return scores
+
+# evaluate a model and try to trap errors and and hide warnings
+def robust_evaluate_model(X, y, model, folds, metric):
+    scores = None
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore")
+            scores = evaluate_model(X, y, model, folds, metric)
+    except:
+        scores = None
+    return scores
+
+# evaluate a dict of models {name:object}, returns {name:score}
+def evaluate_models(X, y, models, folds=10, metric='accuracy'):
+    results = dict()
+    for name, model in models.items():
+        # evaluate the model
+        scores = robust_evaluate_model(X, y, model, folds, metric)
+        # show process
+        if scores is not None:
+            # store a result
+            results[name] = scores
+            mean_score, std_score = np.mean(scores), np.std(scores)
+            print('>%s: %.3f (+/-%.3f)' % (name, mean_score, std_score))
+        else:
+            print('>%s: error' % name)
+    return results
+
+# print and plot the top n results
+def summarize_results(results, maximize=True, top_n=10):
+    # check for no results
+    if len(results) == 0:
+        print('no results')
+        return
+    # determine how many results to summarize
+    n = min(top_n, len(results))
+    # create a list of (name, mean(scores)) tuples
+    mean_scores = [(k,np.mean(v)) for k,v in results.items()]
+    # sort tuples by mean score
+    mean_scores = sorted(mean_scores, key=lambda x: x[1])
+    # reverse for descending order (e.g. for accuracy)
+    if maximize:
+        mean_scores = list(reversed(mean_scores))
+    # retrieve the top n for summarization
+    names = [x[0] for x in mean_scores[:n]]
+    scores = [results[x[0]] for x in mean_scores[:n]]
+    # print the top n
+    print()
+    for i in range(n):
+        name = names[i]
+        mean_score, std_score = mean(results[name]), std(results[name])
+        print('Rank=%d, Name=%s, Score=%.3f (+/- %.3f)' % (i+1, name, mean_score, std_score))
+    # boxplot for the top n
+    pyplot.boxplot(scores, labels=names)
+    _, labels = pyplot.xticks()
+    pyplot.setp(labels, rotation=90)
+    #pyplot.savefig('spotcheck.png')
